077f5e4b49fed27b6e3c82076a1747e7eeaa82a0
Andres Salazar <andresfelipe.salazar@rocketfy.co>
2025-04-01 14:18:20 -0500

    feat: sonar

--- src/main/java/com/example/laboratorio/services/interfaces/IPruebaInspeccionServices.java
+++
@@ -1,9 +0,0 @@
-package com.example.laboratorio.services.interfaces;
-
-import com.example.laboratorio.dto.request.RequestPruebaInspeccion;
-import com.example.laboratorio.dto.response.ResponsePruebaInspeccionDTO;
-
-public interface IPruebaInspeccionServices {
-    ResponsePruebaInspeccionDTO registrar(RequestPruebaInspeccion request);
-    ResponsePruebaInspeccionDTO obtener(int id);
-}
